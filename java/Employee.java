57025146304238a3b1e62fd99cf8e2189d09f566
George Czabania <george@czabania.com>
2013-10-23 08:37:22 +1300

    Add everything back in

---
+++ java/Employee.java
@@ -0,0 +1,131 @@
+
+public class Employee {
+
+  private int    id;
+  private int    age;
+  private int    duties;
+  private char   gender;
+  private String name;
+
+  public Employee () {
+    id     = -1;
+    age    = -1;
+    duties = -1;
+    gender = 'M';
+    name   = "";
+  }
+
+  public Employee (int _id, String _name, char _gender, int _age, int _duties) {
+    id     = _id;
+    name   = _name;
+    gender = _gender;
+    age    = _age;
+    age    = _age;
+    duties = _duties;
+  }
+
+  /**
+   * Convert the employee into a table row
+   */
+
+  public String[] asRow () {
+    return new String[] {
+      Integer.toString(id),
+      name,
+      Character.toString(gender),
+      Integer.toString(age),
+      Integer.toString(duties)
+    };
+  }
+
+  /**
+   * Get id
+   * @return id
+   */
+
+  public int getId () {
+    return this.id;
+  }
+
+  /**
+   * Set id
+   * @param id
+   */
+
+  public void setId (int _id) {
+    this.id = _id;
+  }
+
+  /**
+   * Get age
+   * @return age
+   */
+
+  public int getAge () {
+    return this.age;
+  }
+
+  /**
+   * Set age
+   * @param age
+   */
+
+  public void setAge (int _age) {
+    this.age = _age;
+  }
+
+  /**
+   * Get duties
+   * @return duties
+   */
+
+  public int getDuties () {
+    return this.duties;
+  }
+
+  /**
+   * Set duties
+   * @param duties
+   */
+
+  public void setDuties (int _duties) {
+    this.duties = _duties;
+  }
+
+  /**
+   * Get gender
+   * @return gender
+   */
+
+  public char getGender () {
+    return this.gender;
+  }
+
+  /**
+   * Set gender
+   * @param gender
+   */
+
+  public void setGender (char _gender) {
+    this.gender = _gender;
+  }
+
+  /**
+   * Get name
+   * @return name
+   */
+
+  public String getName () {
+    return this.name;
+  }
+
+  /**
+   * Set name
+   * @param name
+   */
+
+  public void setName (String _name) {
+    this.name = _name;
+  }
+
+}
