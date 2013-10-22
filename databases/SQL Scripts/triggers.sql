77416201036bb2178e1509de8ac9d524e9bc10ef
George Czabania <george@czabania.com>
2013-10-23 12:42:59 +1300

    Fix sql script for sql server

---
+++ databases/SQL Scripts/triggers.sql
@@ -0,0 +1,57 @@
+create table employee (
+	
+	id int identity(1,1) not null,
+	firstname varchar(20) not null,
+	lastname varchar(20) not null,
+	salary float not null,
+	department char(1) not null,
+	
+	constraint pk_employee primary key (id)
+
+);
+
+create table employee_del (
+	
+	id int identity(1,1) not null,
+	firstname varchar(20) not null,
+	lastname varchar(20) not null,
+	salary float not null,
+	department char(1) not null,
+	
+	constraint pk_employee_del primary key (id)
+
+);
+
+create table audit_salary (
+	id int identity(1,1) not null,
+	employee_id int null,
+	username varchar(16) null,
+	date datetime null,
+	salary_old float null,
+	salary_new float null,
+	constraint pk_audit_salary primary key (id)
+);
+
+set identity_insert employee_del on;
+set identity_insert employee on;
+
+create trigger delete_employee on employee after delete as
+begin
+	insert into employee_del (id, firstname, lastname, salary, department) select * from deleted
+end
+
+create trigger update_salary
+on employee after update
+as if update(salary)
+begin
+	declare @salary_old float
+	declare @salary_new float
+	declare @employee int
+	
+	select @salary_old = (select salary from deleted)
+	select @salary_new = (select salary from inserted)
+	select @employee   = (select id from deleted)
+	
+	insert into audit_salary (employee_id, username, date, salary_old, salary_new)
+	values (@employee, user_name(), getdate(), @salary_old, @salary_new)
+end
--- databases/assignment.sql
+++ databases/assignment.sql
@@ -2,185 +2,170 @@
  * CREATE TABLES
  */
 
-CREATE TABLE `Location` (
-  `ID`   int(8)      NOT NULL  AUTO_INCREMENT,
-  `Name` varchar(30) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+CREATE TABLE Location (
+  ID   int      NOT NULL  IDENTITY(1,1),
+  Name varchar(30) NOT NULL,
+  CONSTRAINT pk_Location PRIMARY KEY (ID)
+);
 
-CREATE TABLE `Employer` (
-  `ID`         int(8)      NOT NULL  AUTO_INCREMENT,
-  `Name`       varchar(30) NOT NULL,
-  `LocationID` int(8)      NOT NULL,
-  PRIMARY KEY (`ID`),
-  CONSTRAINT `fk_Employer_LocationID` FOREIGN KEY (`LocationID`)
-  REFERENCES `Location` (`ID`) ON DELETE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+CREATE TABLE Employer (
+  ID         int      NOT NULL  IDENTITY(1,1),
+  Name       varchar(30) NOT NULL,
+  LocationID int      NOT NULL,
+  CONSTRAINT pk_Employer PRIMARY KEY (ID),
+  CONSTRAINT fk_Employer_LocationID FOREIGN KEY (LocationID)
+  REFERENCES Location (ID) ON DELETE NO ACTION
+);
 
-CREATE TABLE `Vacancy` (
-  `ID`          int(8)       NOT NULL  AUTO_INCREMENT,
-  `Description` varchar(200) NOT NULL,
-  `EmployerID`  int(8)       NOT NULL,
-  `LocationID`  int(8)       NOT NULL,
-  `DateCreated` timestamp    NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-  `Type`        tinyint(1)   NOT NULL,
-  PRIMARY KEY (`ID`),
-  CONSTRAINT `fk_Vacancy_EmployerID` FOREIGN KEY (`EmployerID`)
-  REFERENCES `Employer` (`ID`) ON DELETE NO ACTION,
-  CONSTRAINT `fk_Vacancy_LocationID` FOREIGN KEY (`LocationID`)
-  REFERENCES `Location` (`ID`) ON DELETE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+CREATE TABLE Vacancy (
+  ID          int       NOT NULL  IDENTITY(1,1),
+  Description varchar(200) NOT NULL,
+  EmployerID  int       NOT NULL,
+  LocationID  int       NOT NULL,
+  DateCreated datetime    NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+  Type        tinyint   NOT NULL,
+  CONSTRAINT pk_Vacancy PRIMARY KEY (ID),
+  CONSTRAINT fk_Vacancy_EmployerID FOREIGN KEY (EmployerID)
+  REFERENCES Employer (ID) ON DELETE NO ACTION,
+  CONSTRAINT fk_Vacancy_LocationID FOREIGN KEY (LocationID)
+  REFERENCES Location (ID) ON DELETE NO ACTION
+);
 
-CREATE TABLE `Student` (
-  `ID`        int(8)       NOT NULL  AUTO_INCREMENT,
-  `FirstName` varchar(30)  NOT NULL,
-  `LastName`  varchar(30)  NOT NULL,
-  `Address`   varchar(100) NOT NULL,
-  `Phone`     varchar(20)  NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+CREATE TABLE Student (
+  ID        int       NOT NULL  IDENTITY(1,1),
+  FirstName varchar(30)  NOT NULL,
+  LastName  varchar(30)  NOT NULL,
+  Address   varchar(100) NOT NULL,
+  Phone     varchar(20)  NOT NULL,
+  CONSTRAINT pk_Student PRIMARY KEY (ID)
+);
 
-CREATE TABLE `Skill` (
-  `ID`   int(8)      NOT NULL  AUTO_INCREMENT,
-  `Name` varchar(20) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+CREATE TABLE Skill (
+  ID   int      NOT NULL  IDENTITY(1,1),
+  Name varchar(20) NOT NULL,
+  CONSTRAINT pk_Skill PRIMARY KEY (ID)
+);
 
-CREATE TABLE `StudentSkill` (
-  `SkillID`   int(8) NOT NULL,
-  `StudentID` int(8) NOT NULL,
-  PRIMARY KEY (`SkillID`,`StudentID`),
-  CONSTRAINT `fk_StudentSkill_SkillID` FOREIGN KEY (`SkillID`)
-  REFERENCES `Skill` (`ID`) ON DELETE CASCADE,
-  CONSTRAINT `fk_StudentSkill_StudentID` FOREIGN KEY (`StudentID`)
-  REFERENCES `Student` (`ID`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE StudentSkill (
+  SkillID   int NOT NULL,
+  StudentID int NOT NULL,
+  CONSTRAINT pk_StudentSkill PRIMARY KEY (SkillID,StudentID),
+  CONSTRAINT fk_StudentSkill_SkillID FOREIGN KEY (SkillID)
+  REFERENCES Skill (ID) ON DELETE CASCADE,
+  CONSTRAINT fk_StudentSkill_StudentID FOREIGN KEY (StudentID)
+  REFERENCES Student (ID) ON DELETE CASCADE
+);
 
-CREATE TABLE `VacancySkill` (
-  `SkillID`   int(8) NOT NULL,
-  `VacancyID` int(8) NOT NULL,
-  PRIMARY KEY (`SkillID`,`VacancyID`),
-  CONSTRAINT `fk_VacancySkill_SkillID` FOREIGN KEY (`SkillID`)
-  REFERENCES `Skill` (`ID`) ON DELETE CASCADE,
-  CONSTRAINT `fk_VacancySkill_VacancyID` FOREIGN KEY (`VacancyID`)
-  REFERENCES `Vacancy` (`ID`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE VacancySkill (
+  SkillID   int NOT NULL,
+  VacancyID int NOT NULL,
+  CONSTRAINT pk_VacancySkill PRIMARY KEY (SkillID,VacancyID),
+  CONSTRAINT fk_VacancySkill_SkillID FOREIGN KEY (SkillID)
+  REFERENCES Skill (ID) ON DELETE CASCADE,
+  CONSTRAINT fk_VacancySkill_VacancyID FOREIGN KEY (VacancyID)
+  REFERENCES Vacancy (ID) ON DELETE CASCADE
+);
 
-CREATE TABLE `Applicant` (
-  `StudentID`   int(8)    NOT NULL,
-  `VacancyID`   int(8)    NOT NULL,
-  `DateCreated` timestamp NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`StudentID`,`VacancyID`),
-  CONSTRAINT `fk_Applicant_StudentID` FOREIGN KEY (`StudentID`)
-  REFERENCES `Student` (`ID`) ON DELETE CASCADE,
-  CONSTRAINT `fk_Applicant_VacancyID` FOREIGN KEY (`VacancyID`)
-  REFERENCES `Vacancy` (`ID`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE Applicant (
+  StudentID   int    NOT NULL,
+  VacancyID   int    NOT NULL,
+  DateCreated datetime NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT pk_Applicant PRIMARY KEY (StudentID,VacancyID),
+  CONSTRAINT fk_Applicant_StudentID FOREIGN KEY (StudentID)
+  REFERENCES Student (ID) ON DELETE CASCADE,
+  CONSTRAINT fk_Applicant_VacancyID FOREIGN KEY (VacancyID)
+  REFERENCES Vacancy (ID) ON DELETE CASCADE
+);
 
 /**
  * INSERT DATA
  */
 
-INSERT INTO `Location` (`ID`, `Name`)
-VALUES
-  (1,'Mars'),
-  (2,'Atlantis'),
-  (3,'Lynmore'),
-  (4,'Auckland'),
-  (5,'Rotorua'),
-  (6,'Titan'),
-  (7,'Jotunheim'),
-  (8,'Germany'),
-  (9,'Connecticut'),
-  (10,'Australia');
+INSERT INTO Location (Name) VALUES ('Mars');
+INSERT INTO Location (Name) VALUES ('Atlantis');
+INSERT INTO Location (Name) VALUES ('Lynmore');
+INSERT INTO Location (Name) VALUES ('Auckland');
+INSERT INTO Location (Name) VALUES ('Rotorua');
+INSERT INTO Location (Name) VALUES ('Titan');
+INSERT INTO Location (Name) VALUES ('Jotunheim');
+INSERT INTO Location (Name) VALUES ('Germany');
+INSERT INTO Location (Name) VALUES ('Connecticut');
+INSERT INTO Location (Name) VALUES ('Australia');
 
-INSERT INTO `Employer` (`ID`, `Name`, `LocationID`)
-VALUES
-  (1,'Doctor Doom',1),
-  (2,'Dark Phoenix',2),
-  (3,'Thanos',3),
-  (4,'Loki',4),
-  (5,'Magneto',5),
-  (6,'Norman Osborne',6),
-  (7,'Red Skull',7),
-  (8,'Galactus',8),
-  (9,'Ultron',9),
-  (10,'Doctor Octopus',10);
+INSERT INTO Employer (Name, LocationID) VALUES ('Doctor Doom',1);
+INSERT INTO Employer (Name, LocationID) VALUES ('Dark Phoenix',2);
+INSERT INTO Employer (Name, LocationID) VALUES ('Thanos',3);
+INSERT INTO Employer (Name, LocationID) VALUES ('Loki',4);
+INSERT INTO Employer (Name, LocationID) VALUES ('Magneto',5);
+INSERT INTO Employer (Name, LocationID) VALUES ('Norman Osborne',6);
+INSERT INTO Employer (Name, LocationID) VALUES ('Red Skull',7);
+INSERT INTO Employer (Name, LocationID) VALUES ('Galactus',8);
+INSERT INTO Employer (Name, LocationID) VALUES ('Ultron',9);
+INSERT INTO Employer (Name, LocationID) VALUES ('Doctor Octopus',10);
 
-INSERT INTO `Vacancy` (`ID`, `Description`, `EmployerID`, `LocationID`, `DateCreated`, `Type`)
-VALUES
-  (1,'Reporter',1,1,'2013-10-22 18:45:00',1),
-  (2,'Photographer',2,2,'2013-10-22 18:46:00',0),
-  (3,'Police Officer',3,3,'2013-10-22 18:57:41',1),
-  (4,'Forensic Scientist',4,4,'2013-10-22 19:37:49',1),
-  (5,'Detective',5,5,'2013-10-22 19:38:03',5),
-  (6,'Attorney',6,6,'2013-10-22 19:38:12',6),
-  (7,'Scientist',7,7,'2013-10-22 19:38:42',7),
-  (8,'Fire Fighter',8,8,'2013-10-22 19:39:32',8),
-  (9,'Lawyer',9,9,'2013-10-22 19:41:42',9),
-  (10,'Teacher',10,10,'2013-10-22 19:41:53',10);
+INSERT INTO Vacancy (Description, EmployerID, LocationID, DateCreated, Type) VALUES ('Reporter',1,1,'2013-10-22 18:45:00',1);
+INSERT INTO Vacancy (Description, EmployerID, LocationID, DateCreated, Type) VALUES ('Photographer',2,2,'2013-10-22 18:46:00',0);
+INSERT INTO Vacancy (Description, EmployerID, LocationID, DateCreated, Type) VALUES ('Police Officer',3,3,'2013-10-22 18:57:41',1);
+INSERT INTO Vacancy (Description, EmployerID, LocationID, DateCreated, Type) VALUES ('Forensic Scientist',4,4,'2013-10-22 19:37:49',1);
+INSERT INTO Vacancy (Description, EmployerID, LocationID, DateCreated, Type) VALUES ('Detective',5,5,'2013-10-22 19:38:03',5);
+INSERT INTO Vacancy (Description, EmployerID, LocationID, DateCreated, Type) VALUES ('Attorney',6,6,'2013-10-22 19:38:12',6);
+INSERT INTO Vacancy (Description, EmployerID, LocationID, DateCreated, Type) VALUES ('Scientist',7,7,'2013-10-22 19:38:42',7);
+INSERT INTO Vacancy (Description, EmployerID, LocationID, DateCreated, Type) VALUES ('Fire Fighter',8,8,'2013-10-22 19:39:32',8);
+INSERT INTO Vacancy (Description, EmployerID, LocationID, DateCreated, Type) VALUES ('Lawyer',9,9,'2013-10-22 19:41:42',9);
+INSERT INTO Vacancy (Description, EmployerID, LocationID, DateCreated, Type) VALUES ('Teacher',10,10,'2013-10-22 19:41:53',10);
 
-INSERT INTO `Student` (`ID`, `FirstName`, `LastName`, `Address`, `Phone`)
-VALUES
-  (1,'Clark','Kent','Metropolis','12345'),
-  (2,'Hal','Jordon','Coast city','51232'),
-  (3,'Peter','Parker','New York','12354'),
-  (4,'Wally','West','Keystone City','23422'),
-  (5,'Bruce','Wayne','Gotham City','42352'),
-  (6,'Bruce','Banner','New York','52313'),
-  (7,'James','Howlett','Canada','12314'),
-  (8,'Steven','Rogers','New York','12312'),
-  (9,'Tony','Stark','New York','41231'),
-  (10,'Wade','Wilson','Canada','12511');
+INSERT INTO Student (FirstName, LastName, Address, Phone) VALUES ('Clark','Kent','Metropolis','12345');
+INSERT INTO Student (FirstName, LastName, Address, Phone) VALUES ('Hal','Jordon','Coast city','51232');
+INSERT INTO Student (FirstName, LastName, Address, Phone) VALUES ('Peter','Parker','New York','12354');
+INSERT INTO Student (FirstName, LastName, Address, Phone) VALUES ('Wally','West','Keystone City','23422');
+INSERT INTO Student (FirstName, LastName, Address, Phone) VALUES ('Bruce','Wayne','Gotham City','42352');
+INSERT INTO Student (FirstName, LastName, Address, Phone) VALUES ('Bruce','Banner','New York','52313');
+INSERT INTO Student (FirstName, LastName, Address, Phone) VALUES ('James','Howlett','Canada','12314');
+INSERT INTO Student (FirstName, LastName, Address, Phone) VALUES ('Steven','Rogers','New York','12312');
+INSERT INTO Student (FirstName, LastName, Address, Phone) VALUES ('Tony','Stark','New York','41231');
+INSERT INTO Student (FirstName, LastName, Address, Phone) VALUES ('Wade','Wilson','Canada','12511');
 
-INSERT INTO `Skill` (`ID`, `Name`)
-VALUES
-  (1,'Superhuman Strength'),
-  (2,'Invulnerability'),
-  (3,'Self Healing'),
-  (4,'Flight'),
-  (5,'Superhuman Speed'),
-  (6,'X-Ray Vision'),
-  (7,'Quantum Tunneling'),
-  (8,'Wall Crawling'),
-  (9,'Spider Sense'),
-  (10,'Power Ring');
+INSERT INTO Skill (Name) VALUES ('Superhuman Strength');
+INSERT INTO Skill (Name) VALUES ('Invulnerability');
+INSERT INTO Skill (Name) VALUES ('Self Healing');
+INSERT INTO Skill (Name) VALUES ('Flight');
+INSERT INTO Skill (Name) VALUES ('Superhuman Speed');
+INSERT INTO Skill (Name) VALUES ('X-Ray Vision');
+INSERT INTO Skill (Name) VALUES ('Quantum Tunneling');
+INSERT INTO Skill (Name) VALUES ('Wall Crawling');
+INSERT INTO Skill (Name) VALUES ('Spider Sense');
+INSERT INTO Skill (Name) VALUES ('Power Ring');
 
-INSERT INTO `StudentSkill` (`SkillID`, `StudentID`)
-VALUES
-  (1,1),
-  (2,2),
-  (3,3),
-  (4,4),
-  (5,5),
-  (6,6),
-  (7,7),
-  (8,8),
-  (9,9),
-  (10,10);
+INSERT INTO StudentSkill (SkillID, StudentID) VALUES (1,1);
+INSERT INTO StudentSkill (SkillID, StudentID) VALUES (2,2);
+INSERT INTO StudentSkill (SkillID, StudentID) VALUES (3,3);
+INSERT INTO StudentSkill (SkillID, StudentID) VALUES (4,4);
+INSERT INTO StudentSkill (SkillID, StudentID) VALUES (5,5);
+INSERT INTO StudentSkill (SkillID, StudentID) VALUES (6,6);
+INSERT INTO StudentSkill (SkillID, StudentID) VALUES (7,7);
+INSERT INTO StudentSkill (SkillID, StudentID) VALUES (8,8);
+INSERT INTO StudentSkill (SkillID, StudentID) VALUES (9,9);
+INSERT INTO StudentSkill (SkillID, StudentID) VALUES (10,10);
 
-INSERT INTO `VacancySkill` (`SkillID`, `VacancyID`)
-VALUES
-  (1,1),
-  (2,2),
-  (3,3),
-  (4,4),
-  (5,5),
-  (6,6),
-  (7,7),
-  (8,8),
-  (9,9),
-  (10,10);
+INSERT INTO VacancySkill (SkillID, VacancyID) VALUES (1,1);
+INSERT INTO VacancySkill (SkillID, VacancyID) VALUES (2,2);
+INSERT INTO VacancySkill (SkillID, VacancyID) VALUES (3,3);
+INSERT INTO VacancySkill (SkillID, VacancyID) VALUES (4,4);
+INSERT INTO VacancySkill (SkillID, VacancyID) VALUES (5,5);
+INSERT INTO VacancySkill (SkillID, VacancyID) VALUES (6,6);
+INSERT INTO VacancySkill (SkillID, VacancyID) VALUES (7,7);
+INSERT INTO VacancySkill (SkillID, VacancyID) VALUES (8,8);
+INSERT INTO VacancySkill (SkillID, VacancyID) VALUES (9,9);
+INSERT INTO VacancySkill (SkillID, VacancyID) VALUES (10,10);
 
-INSERT INTO `Applicant` (`StudentID`, `VacancyID`, `DateCreated`)
-VALUES
-  (1,1,'2013-10-22 19:42:18'),
-  (2,2,'2013-10-22 19:42:20'),
-  (3,3,'2013-10-22 19:42:23'),
-  (4,4,'2013-10-22 19:42:25'),
-  (5,5,'2013-10-22 19:42:29'),
-  (6,6,'2013-10-22 19:42:33'),
-  (7,7,'2013-10-22 19:42:35'),
-  (8,8,'2013-10-22 19:42:39'),
-  (9,9,'2013-10-22 19:42:41'),
-  (10,10,'2013-10-22 19:42:43');
+INSERT INTO Applicant (StudentID, VacancyID, DateCreated) VALUES (1,1,'2013-10-22 19:42:18');
+INSERT INTO Applicant (StudentID, VacancyID, DateCreated) VALUES (2,2,'2013-10-22 19:42:20');
+INSERT INTO Applicant (StudentID, VacancyID, DateCreated) VALUES (3,3,'2013-10-22 19:42:23');
+INSERT INTO Applicant (StudentID, VacancyID, DateCreated) VALUES (4,4,'2013-10-22 19:42:25');
+INSERT INTO Applicant (StudentID, VacancyID, DateCreated) VALUES (5,5,'2013-10-22 19:42:29');
+INSERT INTO Applicant (StudentID, VacancyID, DateCreated) VALUES (6,6,'2013-10-22 19:42:33');
+INSERT INTO Applicant (StudentID, VacancyID, DateCreated) VALUES (7,7,'2013-10-22 19:42:35');
+INSERT INTO Applicant (StudentID, VacancyID, DateCreated) VALUES (8,8,'2013-10-22 19:42:39');
+INSERT INTO Applicant (StudentID, VacancyID, DateCreated) VALUES (9,9,'2013-10-22 19:42:41');
+INSERT INTO Applicant (StudentID, VacancyID, DateCreated) VALUES (10,10,'2013-10-22 19:42:43');
+
